5a898174f9634989435a3f81c78d29b9ddc3ac90
Glassig <angelina.v.gegerfelt@gmail.com>
2019-12-31 18:38:25 +0100

    Moved homepage url

---
+++ docs/precache-manifest.6baa478f09f5274ddb36ba319597b184.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47209aef2ef35ac6da89de310a15af5",
+    "url": "/newyear/index.html"
+  },
+  {
+    "revision": "02307ecefc126fb7994e",
+    "url": "/newyear/static/css/main.63ab1672.chunk.css"
+  },
+  {
+    "revision": "5f51fd0d9d3882f5e371",
+    "url": "/newyear/static/js/2.5765fe9b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/newyear/static/js/2.5765fe9b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02307ecefc126fb7994e",
+    "url": "/newyear/static/js/main.05199a35.chunk.js"
+  },
+  {
+    "revision": "f9a6984f56eb7c158b9d",
+    "url": "/newyear/static/js/runtime-main.066d80aa.js"
+  },
+  {
+    "revision": "3469563fdf83318365adb30e5609647d",
+    "url": "/newyear/static/media/Button.3469563f.svg"
+  },
+  {
+    "revision": "b11d98f5d3971f056b6cd1a3c853a056",
+    "url": "/newyear/static/media/Logo.b11d98f5.svg"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/newyear/static/media/fa-regular-400.9efb8697.woff2"
+  }
+]);
